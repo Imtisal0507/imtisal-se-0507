d1b077a38dcc89824bd43370294994c22ddfe3dc
Ansari imtisal ahemad <ansariimtisal@gmail.com>
2024-08-10 16:23:47 +0530

    Update question 6.2.c

--- assignment/assignment 2 (module 3)/module 3.1/question 6.2.c
+++ assignment/assignment 2 (module 3)/module 3.1/question 6.2.c
@@ -1,9 +1,11 @@
+//Write a program to convert years into days and days into years  
+
 #include<stdio.h>
 
 main()
 { 
 int year; 
-printf("enter year  : ");
+printf("enter year  : ");  // user defined value
 scanf("%d",&year);
 
 printf("\ndays are : %d",year*365);
