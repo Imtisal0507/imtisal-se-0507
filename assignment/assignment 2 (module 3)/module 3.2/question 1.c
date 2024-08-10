a19d0274a8fc99d047ebc491fff2b51fa40e92f7
Ansari imtisal ahemad <ansariimtisal@gmail.com>
2024-08-10 16:30:30 +0530

    Update question 1.c

--- assignment/assignment 2 (module 3)/module 3.2/question 1.c
+++ assignment/assignment 2 (module 3)/module 3.2/question 1.c
@@ -1,16 +1,17 @@
+//WRITE A PROGRAM to make simple calculator 
 #include<stdio.h>
 main()
 {  
 
     int n1,n2,sum=0; 
-	printf("Enter number :");
+	printf("Enter number :");  
 	scanf("%d",&n1);
 	printf("Enter number :");
 	scanf("%d",&n2);
 	
-	printf("\nAddition of number:%d",sum=n1+n2);
-	printf("\nSubtraction of number:%d",sum=n1-n2);
-	printf("\nMultiplication of number:%d",sum=n1*n2);
-	printf("\nDivision of number:%.2f",(float)n1/n2);
-	printf("\nModulo of number:%d",sum=n2%n1);	
+	printf("\nAddition of number:%d",sum=n1+n2);   // Addition
+	printf("\nSubtraction of number:%d",sum=n1-n2);//  substraction
+	printf("\nMultiplication of number:%d",sum=n1*n2);  //multiplication
+	printf("\nDivision of number:%.2f",(float)n1/n2);  //division
+	printf("\nModulo of number:%d",sum=n2%n1);	//modulo
 }
