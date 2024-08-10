ca45dad02a2949480e4789ae05b4105756b47c46
Ansari imtisal ahemad <ansariimtisal@gmail.com>
2024-08-10 16:42:16 +0530

    Update question 7.c

--- assignment/assignment 2 (module 3)/module 3.2/question 7.c
+++ assignment/assignment 2 (module 3)/module 3.2/question 7.c
@@ -1,3 +1,4 @@
+//Write a program to print Fibonacci series up to given numbers  
 #include<stdio.h>
 main()
 {
@@ -5,10 +6,10 @@ main()
 	printf("Enter a number : ");
 	scanf("%d",&n);
 	
-	printf("%d",n1);
+	printf("%d",n1);           // n1 and n2 is constant in febonacci series
 	printf("\n%d",n2);
 	
-	for(i=3;i<=n;i++)
+	for(i=3;i<=n;i++)             // febonacci is starting is 3
 	{
 		n3=n1+n2;
 		printf("\n%d",n3);
