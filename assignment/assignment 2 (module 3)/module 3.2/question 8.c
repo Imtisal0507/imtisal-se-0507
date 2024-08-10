b8f553b4cfd55512f8f347287cf3dc848adf56a0
Ansari imtisal ahemad <ansariimtisal@gmail.com>
2024-08-10 16:43:11 +0530

    Update question 8.c

--- assignment/assignment 2 (module 3)/module 3.2/question 8.c
+++ assignment/assignment 2 (module 3)/module 3.2/question 8.c
@@ -1,8 +1,9 @@
+//Write a program to print number in reverse order 
 #include<stdio.h>
 main()
 {
 	int i;
-	for (i=82746;i>=64728;i--)
+	for (i=82746;i>=64728;i--)     //loop for write in reverse order
 	{
 		printf("Number is : %d\n",i);
 	}
